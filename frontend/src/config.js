03815a426f2ac84c0a4d9815ff6956430df79f6d
edulopezmartinez <39490086+edulopezmartinez@users.noreply.github.com>
2018-06-19 17:39:01 +0200

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,1 @@
-export const myConfig = { url: 'http://pitufos-elb-364083634.eu-west-1.elb.amazonaws.com:8000' };
+export const myConfig = { url: 'pitufos-app-elb-205285375.eu-west-1.elb.amazonaws.com:8000' };
