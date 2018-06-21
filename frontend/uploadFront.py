c81e601674890425549f435dfeba1e387d2b12e3
edulopezmartinez <39490086+edulopezmartinez@users.noreply.github.com>
2018-06-21 12:35:48 +0200

    Update uploadFront.py

--- frontend/uploadFront.py
+++ frontend/uploadFront.py
@@ -4,7 +4,6 @@ file = open("imagen.txt", "r")
 image = file.read()
 print (image)
 image2 =image.rstrip()
-
 os.system("sudo docker tag %s pitufosgraduates/%s" %(image2, image2))
 #print("docker tag %s pitufosgraduates/%s" %(image2, image2))
 os.system("sudo docker push pitufosgraduates/%s" %image2)
