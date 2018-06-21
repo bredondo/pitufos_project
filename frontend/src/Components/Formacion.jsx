a8537af74b7301ea5caf51960634c5e413f9505c
sergiomarino <sergio.marino@beeva.com>
2018-06-21 15:57:34 +0200

    asfasd

--- frontend/src/Components/Formacion.jsx
+++ frontend/src/Components/Formacion.jsx
@@ -6,7 +6,7 @@ class Formacion extends Component {
   render() {
     return (
         <div>
-            <div className="jumbotron" id="fondoDiv" >
+            <div className="jumbotron" id="fondoDiv">
                 <div className="container" style={{color:'white'}}>
                     <div className="col-md-12">
                         <div className="textForm text-center ">
@@ -28,18 +28,16 @@ class Formacion extends Component {
 
 
 
-            <div style={{margin:'50px'}} className="d-flex justify-content-center">
+            <div style={{backgroundColor:'lightgray'}} className="d-flex justify-content-center">
                 <div className="row">
-
-
                     <div className="col-md-6">
                         <div class="card" style={{height:'auto'}}>
                               <div class="card-body">
                                 <h6 class="card-text text-center">Consulta tu</h6>
                                 <h2 class="card-title text-center">HORARIO</h2>
                                 <hr/>
-                                <a href="http://localhost:8000/horario.pdf" className="btn btn-outline-dark btn-sm btn-block">Ver</a>
-                                <a href="http://localhost:8000/horario.xlsx" className="btn btn-outline-dark btn-sm btn-block">Descargar</a>
+                                <a href="http://localhost:8000/horario.pdf" className="btn btn-outline-dark btn-sm btn-block"><i class="fas fa-file-pdf"></i> Ver</a>
+                                <a href="http://localhost:8000/horario.xlsx" className="btn btn-outline-dark btn-sm btn-block"><i class="fas fa-file-excel"></i> Descargar</a>
                               </div>
                         </div>
                     </div>
@@ -53,18 +51,14 @@ class Formacion extends Component {
                                 <h2 class="card-title text-center">TEMARIO</h2>
                                 <hr></hr>
                                 <ul>    Ponte al día con las teconolgías del momento
-                                        <li id="lista_custom">Vue, React, Polymer</li>
-                                        <li id="lista_custom">Flask, Pymongo, Spring, NodeJS</li>
+                                        <li><i class="fas fa-check"></i>Vue, React, Polymer</li>
+                                        <li><i class="fas fa-check"></i>Flask, Pymongo, Spring, NodeJS</li>
                                 </ul>
-                                <a href="#" class="btn btn-outline-dark btn-sm btn-block">Ver</a>
-                                <a href="#" class="btn btn-outline-dark btn-sm btn-block">Descargar</a>
+                                     <a href="http://localhost:8000/temario.pdf" className="btn btn-outline-dark btn-sm btn-block"><i class="fas fa-file-pdf"></i> Ver</a>
+                                     <a href="http://localhost:8000/temario.pptx" className="btn btn-outline-dark btn-sm btn-block"><i class="fas fa-file-powerpoint"></i> Descargar</a>
                               </div>
-
                         </div>
                     </div>
-
-
-
                 </div>
             </div>
         </div>
