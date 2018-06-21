07ef078c2f0e4a6606caaf7ec65477a40365570a
edulopezmartinez <39490086+edulopezmartinez@users.noreply.github.com>
2018-06-21 12:34:36 +0200

    Update uploadFront.py

--- frontend/uploadFront.py
+++ frontend/uploadFront.py
@@ -1,21 +1,10 @@
 #!/bin/bash
 import os
-
-
-file = open("/home/eduardolopez/Escritorio/Proyecto_pitufos/Proyecto_pitufos/front/imagen.txt", "r")
-
+file = open("imagen.txt", "r")
 image = file.read()
 print (image)
-
 image2 =image.rstrip()
 
-#docker_tag = "docker tag ", image2 , "pitufosgraduates/", image2
-
-#print (docker_tag)
 os.system("sudo docker tag %s pitufosgraduates/%s" %(image2, image2))
 #print("docker tag %s pitufosgraduates/%s" %(image2, image2))
 os.system("sudo docker push pitufosgraduates/%s" %image2)
-
-#os.system('image2=$(sudo docker images -q | grep -m 1 "")')
-#os.system("echo $image2")
-#os.system('sudo docker tag "%a" ')
