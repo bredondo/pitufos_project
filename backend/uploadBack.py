10642ada58a061b678c5a39a3cd11f8f026b72fc
edulopezmartinez <39490086+edulopezmartinez@users.noreply.github.com>
2018-06-21 10:18:07 +0200

    Add files via upload

---
+++ backend/uploadBack.py
@@ -0,0 +1,27 @@
+#!/bin/bash
+import os
+os.chdir("/home/eduardolopez/Escritorio/Proyecto_pitufos/Proyecto_pitufos/back_flask_pymongo")
+os.system("pwd")
+os.system("docker build --no-cache -t back:dockerfile .")
+os.system("sudo docker login -u pitufosgraduates -p pitufos")
+os.system('sudo docker images -q | grep -m 1 "" > imagen.txt')
+os.system('imagen=$(<imagen.txt)')
+os.system("echo $imagen")
+
+file = open("/home/eduardolopez/Escritorio/Proyecto_pitufos/Proyecto_pitufos/back_flask_pymongo/imagen.txt", "r")
+
+image = file.read()
+print (image)
+
+image2 =image.rstrip()
+
+#docker_tag = "docker tag ", image2 , "pitufosgraduates/", image2
+
+#print (docker_tag)
+os.system("sudo docker tag %s pitufosgraduates/%s" %(image2, image2))
+#print("docker tag %s pitufosgraduates/%s" %(image2, image2))
+os.system("sudo docker push pitufosgraduates/%s" %image2)
+
+#os.system('image2=$(sudo docker images -q | grep -m 1 "")')
+#os.system("echo $image2")
+#os.system('sudo docker tag "%a" ')
