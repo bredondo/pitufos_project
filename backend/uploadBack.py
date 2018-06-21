46cee83411cd393420383f7326bd8f813ed57695
edulopezmartinez <39490086+edulopezmartinez@users.noreply.github.com>
2018-06-21 12:22:18 +0200

    Update uploadBack.py

--- backend/uploadBack.py
+++ backend/uploadBack.py
@@ -1,5 +1,10 @@
 #!/bin/bash
-file = open("/home/eduardolopez/Escritorio/Proyecto_pitufos/Proyecto_pitufos/back_flask_pymongo/imagen.txt", "r")
+import os
+file = open("/pitufos_project/backend/imagen.txt", "r")
 image = file.read()
 print (image)
 image2 =image.rstrip()
+os.system("sudo docker tag %s pitufosgraduates/%s" %(image2, image2))
+#print("docker tag %s pitufosgraduates/%s" %(image2, image2))
+os.system("sudo docker push pitufosgraduates/%s" %image2)
+
